825454d6fcf3f33f66b7060d69b37781999a3573
Rahul Thakur <rahulthakurkumar11@gmail.com>
2026-06-24 21:01:54 +0530

    Day 2 - Added User Registration API

---
+++ src/main/java/com/rahul/irctc/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.rahul.irctc.controller;
+
+import com.rahul.irctc.entity.User;
+import com.rahul.irctc.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+
+
+    }
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user){
+        return userService.registerUser(user);
+    }
+}
--- src/main/java/com/rahul/irctc/service/UserServiceImpl.java
+++ src/main/java/com/rahul/irctc/service/UserServiceImpl.java
@@ -1,10 +1,19 @@
 package com.rahul.irctc.service;
 
 import com.rahul.irctc.entity.User;
+import com.rahul.irctc.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService{
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public User registerUser(User user) {
-        return null;
+        return userRepository.save(user);
     }
 }
