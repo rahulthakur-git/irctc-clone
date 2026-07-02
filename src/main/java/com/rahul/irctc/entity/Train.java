225de58b1b89f066f1111f84a3b6237b8066879b
Rahul Thakur <rahulthakurkumar11@gmail.com>
2026-07-02 22:28:44 +0530

    today we complete schedule entity

--- src/main/java/com/rahul/irctc/entity/Train.java
+++ src/main/java/com/rahul/irctc/entity/Train.java
@@ -3,18 +3,33 @@ package com.rahul.irctc.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Set;
+import jakarta.persistence.FetchType;
+
 @Entity
 @Table(name = "Trains")
 @Data
 public class Train {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id ;
+    private Long id ;
     private String trainName;
     private String trainNumber;
+    private String trainType;
     private String source;
     private String destination;
     private Integer totalSeats;
-    private Integer availableSeats;
+    private LocalTime arrivalTime;
+    private LocalTime departureTime;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<DayOfWeek> runningDays;
+    private List<TrainSchedule> schedule;
+
+
+
 
 }
--- src/main/java/com/rahul/irctc/entity/TrainSchedule.java
+++ src/main/java/com/rahul/irctc/entity/TrainSchedule.java
@@ -1,4 +1,20 @@
 package com.rahul.irctc.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+@Entity
+@Table(name = "train_schedule")
+
 public class TrainSchedule {
+    private GenerationType strategy;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "train_id")
+    private Train  train;
+    private LocalDate journeyDate;
+    private Integer availableSeats;
+    private String status;
 }
