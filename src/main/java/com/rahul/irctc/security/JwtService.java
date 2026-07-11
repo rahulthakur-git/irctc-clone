1d56ed366b94789d0f1acb627f1d01bf21dfada4
Rahul Thakur <rahulthakurkumar11@gmail.com>
2026-07-11 23:11:22 +0530

    Creating JwtSerivce for scurity

---
+++ src/main/java/com/rahul/irctc/security/JwtService.java
@@ -0,0 +1,25 @@
+package com.rahul.irctc.security;
+
+import com.rahul.irctc.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+
+public class JwtService {
+    private static final String SECRET = "MySuperSecretKeyForIRCTCProjectJwtAuthentication123456789";
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
+    public String generateToken(User user){
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()+1000*60*60))
+                .signWith(key)
+                .compact();
+    }
+}
